4152b8b9253950909741171c8bb3077caa3a186b
Chang Shin <aproxacs@gmail.com>
2013-08-16 14:37:24 +0900

    refactoring :  removed opening CSV class from uploads_controller, instead added HashableCSV class inheriting CSV.

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,4 +1,3 @@
-require 'csv'
 class UploadsController < ApplicationController
 	def index
 		@upload = Upload.new
@@ -9,25 +8,25 @@ class UploadsController < ApplicationController
 	end
 
   def challenge_platforms
-    data = CSV.new params[:upload][:data].tempfile
+    data = HashableCSV.new params[:upload][:data].tempfile
     PredictionData.import_challenge_platforms(data.to_hash)
     redirect_to uploads_path
   end
 
   def challenge_technologies
-		data = CSV.new params[:upload][:data].tempfile
+		data = HashableCSV.new params[:upload][:data].tempfile
   	PredictionData.import_challenge_technologies(data.to_hash)
   	redirect_to uploads_path
   end
 
   def challlenges
-		data = CSV.new params[:upload][:data].tempfile
+		data = HashableCSV.new params[:upload][:data].tempfile
   	PredictionData.import_challenges(data.to_hash)
   	redirect_to uploads_path
   end
 
   def challenge_participants
-		data = CSV.new params[:upload][:data].tempfile
+		data = HashableCSV.new params[:upload][:data].tempfile
   	PredictionData.import_challenge_participants(data.to_hash)
   	redirect_to uploads_path, notice: 'Upload completed'
   end
@@ -49,12 +48,4 @@ class UploadsController < ApplicationController
     redirect_to root_path, notice: 'Successfully uploaded Prediction Data'
   end
 
-end
-
-class CSV
-	def to_hash
-		headers = self.shift.map {|i| i.to_s }
-		string_data = self.map {|row| row.map {|cell| cell.to_s } }
-		array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
-	end
 end
